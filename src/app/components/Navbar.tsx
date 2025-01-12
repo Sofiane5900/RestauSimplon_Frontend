85426faf1cc0b722d7483f313d59ce6eb18b98ff
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2025-01-12 17:05:02 +0100

    retouches navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -18,45 +18,49 @@ const Navbar = () => {
   });
 
   return (
-    <nav
-      className={`w-full flex items-center px-8 py-4 justify-between bg-white transition-all duration-300 ${
-        sticky ? "fixed top-0 z-50 shadow-md" : ""
-      }`} // Si sticky est true, on ajoute les classes 'fixed top-0 z-50 shadow-md'
-    >
-      <Image src="./logo.svg" alt="logo" width={200} height={200} />
-      <ul className="hidden gap-3 md:flex md:gap-x-8 ml-auto">
-        <li className="text-teal-950 list-none poppins">Home</li>
-        <li className="text-teal-950 list-none poppins">Menu</li>
-        <li className="text-teal-950 list-none poppins">Contact</li>
-        <button className="text-white bg-teal-950 poppins ">Commande</button>
-      </ul>
-
-      {/* Hamburger Icon */}
-      <button
-        onClick={() => setIsNavOpen(!isNavOpen)}
-        className="md:hidden ml-auto"
+    <>
+      <nav
+        className={`w-full flex items-center px-8 py-4 justify-between bg-white transition-all duration-300 ${
+          sticky ? "fixed top-0 z-50 shadow-md" : ""
+        }`} // Si sticky est true, on ajoute les classes 'fixed top-0 z-50 shadow-md'
       >
-        <span className="block w-6 h-0.5 bg-teal-950 mb-1"></span>
-        <span className="block w-6 h-0.5 bg-teal-950 mb-1"></span>
-        <span className="block w-6 h-0.5 bg-teal-950"></span>
-      </button>
-
-      {/* Mobile Menu */}
-      {isNavOpen && (
-        <div className="md:hidden absolute top-16 left-0 right-0 bg-white shadow-lg p-4">
-          <ul className="flex flex-col items-center gap-y-4">
-            <li className="text-teal-950 list-none poppins">Home</li>
-            <li className="text-teal-950 list-none poppins">Menu</li>
-            <li className="text-teal-950 list-none poppins">Contact</li>
-            <button className="text-white bg-teal-950 poppins py-2 px-4 rounded">
+        <Image src="./logo.svg" alt="logo" width={200} height={200} />
+        <ul className="hidden gap-3 md:flex md:gap-x-8 ml-auto items-center">
+          <li className="text-teal-950 list-none poppins">Home</li>
+          <li className="text-teal-950 list-none poppins">Menu</li>
+          <li className="text-teal-950 list-none poppins">Contact</li>
+          <li>
+            <button className="text-white bg-teal-950 poppins py-2 px-4 rounded hover:scale-110 transition-all duration-200 ease-in-out">
               Commande
             </button>
-          </ul>
-        </div>
-      )}
+          </li>
+        </ul>
+        {/* Hamburger Icon */}
+        <button
+          onClick={() => setIsNavOpen(!isNavOpen)}
+          className="md:hidden ml-auto"
+        >
+          <span className="block w-6 h-0.5 bg-teal-950 mb-1"></span>
+          <span className="block w-6 h-0.5 bg-teal-950 mb-1"></span>
+          <span className="block w-6 h-0.5 bg-teal-950"></span>
+        </button>
 
-      <hr></hr>
-    </nav>
+        {/* Mobile Menu */}
+        {isNavOpen && (
+          <div className="md:hidden absolute top-16 left-0 right-0 bg-white shadow-lg p-4">
+            <ul className="flex flex-col items-center gap-y-4">
+              <li className="text-teal-950 list-none poppins">Home</li>
+              <li className="text-teal-950 list-none poppins">Menu</li>
+              <li className="text-teal-950 list-none poppins">Contact</li>
+              <button className="text-white bg-teal-950 poppins py-2 px-4 rounded">
+                Commande
+              </button>
+            </ul>
+          </div>
+        )}
+      </nav>
+      <hr />
+    </>
   );
 };
 
